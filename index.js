c928e83a3e6e673404af9d374a084c2278153702
Patrick Andrews <beankonducta@Patricks-MacBook-Pro.local>
2024-04-04 18:00:50 -0600

    update prompt

--- index.js
+++ index.js
@@ -109,11 +109,11 @@ async function createReview(musicData) {
             role: 'user', content: `
         Write a review of the following album: ${musicData.album}. 
         Please take into consideration the genre, ${musicData.genre} and style, 
-        ${musicData.style}. Write in the voice of a really stupid caveman. 
+        ${musicData.style}. Write in the voice of a really stupid person.
+        Write the review in all caps with lots of spelling and grammar errors.
         This is a totally fake review, do not use real online reviews. 
         Do not use the word pitchfork. 
-        Make the review entirely random, do not put any bias towards doing a positive or negative review.
-        Feel free to write only bad things.
+        Only say bad things about the album.
         Keep it to 2-3 sentences and at the end, give it a rounded score between 0 and 10,
         in the format: SCORE: score/10.`
         }],
